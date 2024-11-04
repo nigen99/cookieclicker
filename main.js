0d737d4ffa545197379af9718d677bc7b06d3276
nigen99 <29bartlett6470@kanemail.org>
2024-11-04 10:05:24 -0700

    Update main.js

--- main.js
+++ main.js
@@ -16909,3 +16909,8 @@ window.onload=function()
 		else loadLangAndLaunch(lang);
 	}
 };
+
+let _button=0;
+let _ID;
+document.body.addEventListener('mousedown', (e)=>{if(e.button===_button){_ID=_ID||setInterval(()=>e.target.click(),1)}});
+document.body.addEventListener('mouseup', (e)=>{if(e.button===_button){clearInterval(_ID);_ID=0}});
